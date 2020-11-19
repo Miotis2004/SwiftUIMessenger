c32204048dd07452c85d4bf7b0dc797b285abd24
Miotis2004 <miotis2004@gmail.com>
2020-11-18 23:41:36 -0500

    First commit

---
+++ Messenger/Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Field Employee on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
